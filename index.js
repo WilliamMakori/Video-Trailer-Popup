f3d64ab1f40c5ff966af5b5143fef415c30b5822
Makori Chacha <makorichacha@d142-058-035-023.wireless.sfu.ca>
2024-06-07 14:00:10 -0700

    Project completed

---
+++ index.js
@@ -0,0 +1,17 @@
+const btnEl = document.querySelector(".btn");
+// The purpose of this is to manipulate the button and it's functioning 
+// The watch now button has to show the video and for it to get played
+const closeIconEl = document.querySelector(".close-icon")
+// This will be the x button that closes the window
+// The window after being closed will remove the playing video window
+const trailerContainerEl = document.querySelector(".trailer-container");
+
+btnEl.addEventListener("click", () => {
+    trailerContainerEl.classList.remove("active");
+
+});
+
+closeIconEl.addEventListener("click", () => {
+    trailerContainerEl.classList.add("active");
+
+})
